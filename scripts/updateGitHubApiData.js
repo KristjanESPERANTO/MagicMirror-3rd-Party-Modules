c750a032a13630f97dd17ba1c8f8f0f4e3b4bf4d
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-01-30 02:26:53 +0100

    Getting GitHub data

--- scripts/updateGitHubApiData.js
+++ scripts/updateGitHubApiData.js
@@ -1,19 +1,39 @@
 import fs from "node:fs";
+import process from "node:process";
+
+let queryCount = 0;
+let maxQueryCount = 300;
+let moduleCount = 0;
+
+function printProgress (count, total) {
+  process.stdout.cursorTo(0);
+  process.stdout.write(`${count} / ${total}`);
+}
+
+function getJson (filePath) {
+  const data = fs.readFileSync(filePath, "utf8");
+  const json = JSON.parse(data);
+  return json;
+}
 
 // Function to check whether new data should be retrieved.
 function shouldFetch (repository, lastUpdate) {
   let retrieve = false;
-  if (lastUpdate) {
-    const now = new Date();
-    const daysSinceLastUpdate = Math.round((now - new Date(lastUpdate)) / (1000 * 60 * 60 * 24));
-    const lastCommitDate = repository.gitHubData ? new Date(repository.gitHubData.lastCommit) : new Date(-20);
-    const isUpdateLongAgo = daysSinceLastUpdate > 7;
-    const wasLastUpdateBeforeLastCommit = lastUpdate < lastCommitDate.toISOString();
+  if (repository.url.includes("github.com")) {
+    if (queryCount < maxQueryCount) {
+      if (lastUpdate) {
+        const now = new Date();
+        const daysSinceLastUpdate = Math.round((now - new Date(lastUpdate)) / (1000 * 60 * 60 * 24));
+        const lastCommitDate = repository.gitHubData ? new Date(repository.gitHubData.lastCommit) : new Date(-20);
+        const isUpdateLongAgo = daysSinceLastUpdate > 7;
+        const wasLastUpdateBeforeLastCommit = lastUpdate < lastCommitDate.toISOString();
 
-    retrieve = isUpdateLongAgo || wasLastUpdateBeforeLastCommit;
-  } else {
-    // If there is no previous data, always retrieve.
-    retrieve = true;
+        retrieve = isUpdateLongAgo || wasLastUpdateBeforeLastCommit;
+      } else {
+        // If there is no previous data, always retrieve.
+        retrieve = true;
+      }
+    }
   }
   return retrieve;
 }
@@ -25,46 +45,61 @@ async function updateData () {
     try {
       previousData = JSON.parse(fs.readFileSync("docs/data/gitHubData.json"));
     } catch (error) {
-      // The file does not yet exist or could not be read
+      console.error("Error reading previous data:", error);
     }
 
-    const moduleList = JSON.parse(fs.readFileSync("repositories.json"));
+    const moduleListData = await getJson("./docs/data/modules.stage.1.json");
+    const moduleList = moduleListData.modules;
+    const moduleListLength = moduleList.length;
+
     const results = [];
 
     for (const module of moduleList) {
       const repositoryId = module.id;
-      const url = `https://api.github.com/repos/${repositoryId}`;
+      const repositoryApiUrl = `https://api.github.com/repos/${repositoryId}`;
+      moduleCount += 1;
+      printProgress(moduleCount, moduleListLength);
 
       // Check whether the data should be retrieved again
       const lastUpdate = previousData.repositories?.find((repo) => repo.id === repositoryId)?.gitHubDataLastUpdate;
       const shouldFetchData = shouldFetch(module, lastUpdate);
 
       if (shouldFetchData) {
-        const response = await fetch(url);
+        const response = await fetch(repositoryApiUrl);
         const data = await response.json();
+        queryCount += 1;
 
         const branchUrl = `https://api.github.com/repos/${repositoryId}/commits/${data.default_branch}`;
         const branchResponse = await fetch(branchUrl);
         const branchData = await branchResponse.json();
+        queryCount += 1;
 
-        const repositoryData = {
-          "id": module.id,
-          "gitHubDataLastUpdate": new Date().toISOString(),
-          "gitHubData": {
-            "issues": data.open_issues,
-            "stars": data.stargazers_count,
-            "license": data.license ? data.license.spdx_id : null,
-            "archived": data.archived,
-            "disabled": data.disabled,
-            "defaultBranch": data.default_branch,
-            "lastCommit": branchData.commit ? branchData.commit.author.date : null
-          }
-        };
+        if (response.status === 200) {
+          const repositoryData = {
+            "id": module.id,
+            "gitHubDataLastUpdate": new Date().toISOString(),
+            "gitHubData": {
+              "issues": data.open_issues,
+              "stars": data.stargazers_count,
+              "license": data.license ? data.license.spdx_id : null,
+              "archived": data.archived,
+              "disabled": data.disabled,
+              "defaultBranch": data.default_branch,
+              "lastCommit": branchData.commit ? branchData.commit.author.date : null
+            }
+          };
 
-        results.push(repositoryData);
+          results.push(repositoryData);
+        } else {
+          console.error("\nError fetching GitHub API data:", response.status, response.statusText);
+          maxQueryCount = 0;
+        }
       } else {
         // Add the existing data without updating it
-        results.push(previousData.repositories?.find((repo) => repo.id === repositoryId));
+        const existingRepository = previousData.repositories?.find((repo) => repo.id === repositoryId);
+        if (existingRepository) {
+          results.push(existingRepository);
+        }
       }
     }
 
@@ -74,7 +109,7 @@ async function updateData () {
     };
 
     fs.writeFileSync("docs/data/gitHubData.json", JSON.stringify(updateInfo, null, 2));
-    console.info("GitHub data update completed successfully.");
+    console.info("\nGitHub data update completed. queryCount:", queryCount, "maxQueryCount:", maxQueryCount, "results:", results.length, "modules:", moduleListLength);
   } catch (error) {
     console.error("Error fetching GitHub API data:", error);
   }
