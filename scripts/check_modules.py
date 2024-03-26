578652887942ecaef1c1fa3cf22e9217d899c14a
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-03-26 22:30:46 +0100

    Deactivate master check

--- scripts/check_modules.py
+++ scripts/check_modules.py
@@ -488,9 +488,11 @@ def check_branch_name(module, module_directory_path):
         .stdout.decode()
         .rstrip()
     )
-    if "* master" in branch:
-        module["issues"].append(
-            "The branch name is 'master'. Consider renaming it to 'main'.")
+    # Deactivated because it causes issues for the users. They would have to rename the branch, but they probably don't know how to do it.
+    # https://github.com/KristjanESPERANTO/MagicMirror-3rd-Party-Modules/issues/24
+    # if "* master" in branch:
+    #    module["issues"].append(
+    #        "The branch name is 'master'. Consider renaming it to 'main'.")
 
 
 check_modules()
