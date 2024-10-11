25dfaea695dfbbf2aff10f9427838eaecae3e47c
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-10-11 20:26:05 +0200

    Add menu

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,7 @@ const config = [
       "modules/*",
       "modules_temp/*",
       "docs/data/modules*.json",
+      "docs/fonts/*",
       "result.md"
     ]
   },
@@ -36,7 +37,7 @@ const config = [
       "id-length": ["error", {"exceptions": ["a", "b"]}],
       "import-x/no-unresolved": ["error", {"ignore": ["eslint-plugin-package-json/configs/recommended"]}],
       "max-depth": ["warn", 5],
-      "max-lines": ["warn", 450],
+      "max-lines": ["warn", 500],
       "max-lines-per-function": ["warn", 150],
       "max-params": ["warn", 5],
       "max-statements": "off",
