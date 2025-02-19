eb518c7d95855722116f9d8bea4c4c0af4b527aa
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-02-20 00:07:31 +0100

    Add package.json checks to ESLint check config

--- eslint.testconfig.js
+++ eslint.testconfig.js
@@ -1,12 +1,14 @@
 import eslintPluginDepend from "eslint-plugin-depend";
 import eslintPluginImport from "eslint-plugin-import";
 import eslintPluginJs from "@eslint/js";
+import eslintPluginPackageJson from "eslint-plugin-package-json/configs/recommended";
 import globals from "globals";
 
 const config = [
   eslintPluginDepend.configs["flat/recommended"],
   eslintPluginImport.flatConfigs.recommended,
   eslintPluginJs.configs.recommended,
+  eslintPluginPackageJson,
   {
     "ignores": ["**/*.min.js"]
   },
@@ -56,7 +58,9 @@ const config = [
   {
     "files": ["package.json"],
     "rules": {
-      "depend/ban-dependencies": ["error", {"allowed": ["moment", "eslint-plugin-import"]}]
+      "depend/ban-dependencies": ["error", {"allowed": ["moment", "eslint-plugin-import"]}],
+      "package-json/require-author": "error",
+      "package-json/require-keywords": "error"
     }
   }
 ];
