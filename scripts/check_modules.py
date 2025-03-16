4b6947232b5731963de3fb19de2a4e7e5dd1ea74
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-03-16 19:20:39 +0100

    Add minimum default sort weight for modules by specific maintainer

--- scripts/check_modules.py
+++ scripts/check_modules.py
@@ -450,6 +450,10 @@ def check_modules():
                 module["defaultSortWeight"] = max(
                     module["defaultSortWeight"], 1)
 
+            # Just to reduce imbalance in the default sort order, modules from this developer get a minimum value of one.
+            if module['maintainer'] == "KristjanESPERANTO" and module["name"] != "MMM-EasyPix":
+                module["defaultSortWeight"] = max(module["defaultSortWeight"], 1)
+
     print(
         f"{stats['moduleCounter']} modules analyzed. For results see file result.md.           ")
 
